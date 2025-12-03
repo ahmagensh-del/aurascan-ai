13f08975ff6fa62c3892f28ddf35fd4e0b0a2134
ahmagensh-del <ahmagensh@gmail.com>
2025-12-03 22:29:52 +0100

    Update page.js

--- app/paywall/page.js
+++ app/paywall/page.js
@@ -1,5 +1,8 @@
 "use client";
+
 export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+export const revalidate = 0;
 
 import { useRouter, useSearchParams } from "next/navigation";
 
